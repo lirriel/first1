2a3a93380b49bd4ef218c7e72035c136a41cf639
lirriel24 <druidmif@gmail.com>
2017-07-20 16:16:06 +0300

    y

---
+++ Empty/main.js
@@ -0,0 +1,28 @@
+/*window.onload = function(){
+	 // add eventListener for keydown
+    document.addEventListener('keydown', function(e) {
+    	switch(e.keyCode){
+    	case 37: //LEFT arrow
+    		alert("LEFT");
+    		break;
+    	case 38: //UP arrow
+    		alert("UP");
+    		break;
+    	case 39: //RIGHT arrow
+    		alert("RIGHT");
+    		break;
+    	case 40: //DOWN arrow
+    		alert("DOWN");
+    		break;
+    	case 13: //OK button
+    		alert("OK");
+    		break;
+    	case 10009: //RETURN button
+		tizen.application.getCurrentApplication().exit();
+    		break;
+    	default:
+    		console.log('Key code : ' + e.keyCode);
+    		break;
+    	}
+    });
+}*/
